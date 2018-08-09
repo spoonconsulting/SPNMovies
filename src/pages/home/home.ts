b555409b2adcffef44d8c54f848dfa632514fd55
Dinitri Ragoo <dinitri.ragoo@sc-mauritius.com>
2018-08-09 17:32:20 +0400

    started movie cards

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    public movies = [];
 
-  }
+    constructor(public navCtrl: NavController) {
+        this.movies = [{
+                name: "catMovie",
+                posterUrl: "http://thecatapi.com/api/images/get?format=src&type=png&name=est"
+            },
+            {
+                name: "catMovie",
+                posterUrl: "http://thecatapi.com/api/images/get?format=src&type=png&name=tst"
+            }
+        ];
+    }
 
 }
