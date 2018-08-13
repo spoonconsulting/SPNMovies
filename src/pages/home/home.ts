ffeccfdb8717cab09373f3983da24e3fecc3da0e
Dinitri Ragoo <dinitri.ragoo@sc-mauritius.com>
2018-08-13 09:49:21 +0400

    fixed scrolling between segments

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-import { library } from '@fortawesome/fontawesome-svg-core';
-
 
 @Component({
     selector: 'page-home',
@@ -14,8 +11,7 @@ export class HomePage {
     public currentTab="latest";
 
     constructor(public navCtrl: NavController) {
-        library.add(faStar);
-        for(var i of [1,2,3,4,5,6,7,8,9,10]){
+        for(var i of [1,2,3,4,5,6,7,8,9,10,11]){
             var movie = {
                 name: `catMovie ${i}`,
                 posterUrl:`http://thecatapi.com/api/images/get?format=src&type=png&name=est_${i}`
