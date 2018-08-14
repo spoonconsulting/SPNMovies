5d3366d109f6b20bdca2b98302f0bcc54569db94
Dinitri Ragoo <dinitri.ragoo@sc-mauritius.com>
2018-08-14 07:05:06 +0400

    Load movies function including catch error

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { MovieService } from '../../providers/movie-service';
 import { Movie } from '../../models/movie';
 
@@ -9,14 +9,26 @@ import { Movie } from '../../models/movie';
 })
 export class HomePage {
     public currentTab = "latest";
-    public movieList:Movie[];
-    public topRatedMovieList:Movie[];
+    public movieList: Movie[];
+    public topRatedMovieList: Movie[];
 
-    constructor(public navCtrl: NavController, public movieService: MovieService) {
-        this.movieService.getMovies().subscribe(movies=>{
-            console.log("got movies",movies);
-            this.movieList=movies;
-        });
+    constructor(public navCtrl: NavController, public movieService: MovieService, private alertController: AlertController) {
+        this.loadMovies(false);
     }
 
+    private loadMovies(isTopRated) {
+        console.log("loadMovies");
+        this.movieService.getMovies(isTopRated).subscribe(movies => {
+            console.log("loadMovies res tab: ", this.currentTab);
+            if (this.currentTab == "latest") {
+                this.movieList = movies;
+            } else {
+                this.topRatedMovieList = movies;
+            }
+        }, err => this.alertController.create({
+            title: 'Cannot Fetch Movies',
+            subTitle: 'We are sorry our server is under maintenance.',
+            buttons: ['OK']
+        }).present());
+    }
 }
--- src/providers/movie-service.ts
+++ src/providers/movie-service.ts
@@ -1,27 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {Observable} from 'rxjs/Observable'
+import { Observable } from 'rxjs/Observable'
 import { Movie } from '../models/movie';
 
 @Injectable()
 export class MovieService {
-    movieUrl = "https://yts.am/api/v2/list_movies.json";
+    movieUrlByLatest = "https://yts.am/api/v2/list_movies.json";
+    movieUrlByRating = "https://yts.am/api/v2/list_movies.json?sort_by=rating";
+    movieUrl: string;
 
     constructor(public http: HttpClient) {}
 
-    getMovies(): Observable < Movie[] > {
+    getMovies(orderByRating: boolean): Observable < Movie[] > {
+        this.movieUrl =orderByRating?this.movieUrlByRating: this.movieUrlByLatest;
         return new Observable(observer => {
-            this.http.get(this.movieUrl).subscribe((response:any) => {
+            this.http.get(this.movieUrl).subscribe((response: any) => {
                 console.log("response json: ", response);
                 let moviesData = response.data.movies;
+                if (!moviesData)
+                    return observer.error();
                 let moviesToReturn = [];
-                moviesData.forEach(responseMovie => {
-                    moviesToReturn.push(new Movie(responseMovie));
-                });
+                moviesData.forEach(responseMovie => moviesToReturn.push(new Movie(responseMovie)));
                 observer.next(moviesToReturn);
-            });
-        });
+            }, error=> observer.error(error));
 
+        });
     }
 
 }
