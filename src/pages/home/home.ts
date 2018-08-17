f0bb7823b439e75504173e781f28087cbce707b0
Dinitri Ragoo <dinitri.ragoo@sc-mauritius.com>
2018-08-17 12:29:35 +0400

    added loading while fetching movies from web api

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
+import { NavController, AlertController, LoadingController, Loading } from 'ionic-angular';
 import { MovieService } from '../../providers/movie-service';
 import { Movie } from '../../models/movie';
 import { MovieDetailPage } from '../../pages/movie-detail/movie-detail';
@@ -12,29 +12,42 @@ export class HomePage {
     public currentTab = "latest";
     public movieList: Movie[];
     public topRatedMovieList: Movie[];
+    public loading: Loading;
 
     constructor(public navCtrl: NavController,
         public movieService: MovieService,
-        private alertController: AlertController) {
+        private alertController: AlertController,
+        public loadingCtrl: LoadingController) {
         this.movieList = [];
         this.topRatedMovieList = [];
+
         this.loadMovies(false);
     }
 
     private loadMovies(isTopRated) {
+
         if (!this.movieList.length || !this.topRatedMovieList.length) {
+            this.loading = this.loadingCtrl.create({
+                cssClass: 'activity-detail-loading'
+            })
+            this.loading.present();
             this.movieService.getMovies(isTopRated).subscribe(movies => {
+                    this.loading.dismiss();
                     if (this.currentTab == "latest") {
                         this.movieList = movies;
                     } else {
                         this.topRatedMovieList = movies;
                     }
-                }, err =>
-                this.alertController.create({
-                    title: 'Error',
-                    subTitle: 'Unable to fetch movies.\nPlease try again later.',
-                    buttons: ['OK']
-                }).present()
+                },
+                err => {
+                    this.loading.dismiss();
+                    this.alertController.create({
+                        title: 'Error',
+                        subTitle: 'Unable to fetch movies.\nPlease try again later.',
+                        buttons: ['OK']
+                    }, ).present()
+                },
+
             );
         }
     }
