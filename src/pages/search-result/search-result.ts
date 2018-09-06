505a978d4287aa21ac8fe232861e25ddc4c78f2d
Dinitri Ragoo <pmdragoo@yahoo.com>
2018-09-06 16:42:15 +0400

    infinite scrolling

--- src/pages/search-result/search-result.ts
+++ src/pages/search-result/search-result.ts
@@ -2,21 +2,36 @@ import { Component } from '@angular/core';
 import { NavParams, NavController } from 'ionic-angular';
 import { Movie } from '../../models/movie';
 import { MovieDetailPage } from '../../pages/movie-detail/movie-detail';
+import { SearchPage } from '../search/search';
+import { MovieService } from '../../providers/movie-service';
 
 @Component({
-  selector: 'page-search-result',
-  templateUrl: 'search-result.html',
+    selector: 'page-search-result',
+    templateUrl: 'search-result.html',
 })
 export class SearchResult {
 
-  movies: Movie[];
+    movies: Movie[];
+    page:number=1;
 
-  constructor(public navController: NavController,public navParams: NavParams) {
-    this.movies= this.navParams.get('movies');
-  }
+    constructor(public navController: NavController, public navParams: NavParams, public movieService: MovieService) {
+        this.movies = this.navParams.get('movies');
+    }
 
-  private didSelectMovie(movie: Movie){
-    this.navController.push(MovieDetailPage, {"movie":movie});
-  }
-  
+    private didSelectMovie(movie: Movie) {
+        this.navController.push(MovieDetailPage, { "movie": movie });
+    }
+
+    doInfinite(): Promise < any > {
+        console.log('Begin async operation');
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                console.log(this.movieService.queryWording + "#########");
+                this.movieService.searchMovie(this.movieService.queryWording,'','','',++this.page)
+                .subscribe(movies=> movies.forEach(m=>this.movies.push(m)));
+                console.log('Async operation has ended');
+                resolve();
+            }, 500);
+        })
+    }
 }
